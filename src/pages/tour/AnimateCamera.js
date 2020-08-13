6a18099703b24cfd89ada4170e0c9829e5f68785
Chris <clenart4@kent.edu>
2020-08-13 17:09:50 -0400

    fixed issue resulting in camera sticking

--- src/pages/tour/AnimateCamera.js
+++ src/pages/tour/AnimateCamera.js
@@ -6,7 +6,12 @@ const AnimateCamera = ({ isRotating, setIsRoating }) => {
 
     const { camera } = useThree();
 
-    useFrame(() => { if (isRotating) camera.position.set(0, 0, 1) })
+    useFrame(() => {
+        if (isRotating) {
+            camera.position.set(0, 0, 1)
+            setIsRoating(false);
+        }
+    })
 
     return(null)
 };
