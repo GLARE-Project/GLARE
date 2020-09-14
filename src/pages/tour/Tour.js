eb42c9d9428d0514fe2ef59e69edefc6b1b80ccd
Chris <clenart4@kent.edu>
2020-09-14 18:20:50 -0400

    no longer require audio - uses initial state

--- src/pages/tour/Tour.js
+++ src/pages/tour/Tour.js
@@ -24,9 +24,11 @@ const Fallback = () => (
 
 const Tour = ({ history }) => {
     const query = new URLSearchParams(useLocation().search);
+    const INITAL_STATE = { name: "", start_audio: "" };
 
     const { onCampus, markerData } = useContext(Context);
-    const [StorageData, setStoredData] = useState(null);
+    const [StorageData, setStoredData] = useState(INITAL_STATE);
+    const { name, start_audio } = StorageData;
 
     const [isRotating, setIsRoating] = useState(false);
 
@@ -40,57 +42,60 @@ const Tour = ({ history }) => {
     }
 
     useEffect(() => {
-        const name = query.get("name");
-        const data = markerData.filter(marker => marker.name === name).pop();
-        setStoredData(data);
+        const markerName = query.get("name");
+        const data = markerData.filter(marker => marker.name === markerName).pop();
+        // if data hasn't loaded yet, it'll be undefined. So ignore it then and wait for it to load
+        if (typeof data !== 'undefined') {
+            // we make sure there exists INITAL_STATE if they don't exist in the data
+            const mutatedData = { ...INITAL_STATE, ...data };
+            if (JSON.stringify(StorageData) !== JSON.stringify(mutatedData)) setStoredData(mutatedData)
+        }
         // used to get rid of the bar in safari
         scrollToTop();
         scrollToBottom();
-    }, [markerData, query]);
+    }, [markerData, query, StorageData, INITAL_STATE]);
 
-    if (StorageData != null)
-        return (
-            <div id="container" style={{ overflow: "hidden" }}>
-                <MenuOverlay history={history} data={StorageData}>
+    return (
+        <div id="container" style={{ overflow: "hidden" }}>
+            <MenuOverlay history={history} data={StorageData}>
 
-                    {onCampus && (<video autoPlay={true} muted="" playsInline="" id="videoElement" />)}
+                {onCampus && (<video autoPlay={true} muted="" playsInline="" id="videoElement" />)}
 
-                    <Canvas id="canvas" camera={{ position: [0, 0, 1], fov: 45 }}>
-                        <Suspense fallback={<Fallback />}>
-                            {onCampus ?
-                                <SphereMapAR data={StorageData} /> :
-                                <CubeMapVR data={StorageData} />
-                            }
-                            <AnimateCamera isRotating={isRotating} setIsRoating={setIsRoating} />
-                        </Suspense>
-                    </Canvas>
+                <Canvas id="canvas" camera={{ position: [0, 0, 1], fov: 45 }}>
+                    <Suspense fallback={<Fallback />}>
+                        {onCampus ?
+                            <SphereMapAR data={StorageData} /> :
+                            <CubeMapVR data={StorageData} />
+                        }
+                        <AnimateCamera isRotating={isRotating} setIsRoating={setIsRoating} />
+                    </Suspense>
+                </Canvas>
 
-                    <div onSelect={e => e.preventDefault()} id="title-ctn" >
-                        {StorageData.name}
-                    </div>
+                <div onSelect={e => e.preventDefault()} id="title-ctn" >
+                    {name}
+                </div>
 
-                    <div id="fixed-footer">
-                        <AudioPlayer name={StorageData.name} source={StorageData.start_audio} />
-                    </div>
+                <div id="fixed-footer">
+                    <AudioPlayer name={name} source={start_audio} />
+                </div>
 
-                    <div className="overlay-ctn-show">
-                        <FontAwesomeIcon
-                            className="overlay-icon"
-                            icon={faEye}
-                            onClick={() => setIsRoating(true) }
-                        />
-                    </div>
-                    <div className="overlay-ctn-map">
-                        <FontAwesomeIcon
-                            className="overlay-icon"
-                            icon={faMapMarkerAlt}
-                            onClick={() => history.replace("/map")}
-                        />
-                    </div>
-                </MenuOverlay>
-            </div>
-        );
-    else return null
+                <div className="overlay-ctn-show">
+                    <FontAwesomeIcon
+                        className="overlay-icon"
+                        icon={faEye}
+                        onClick={() => setIsRoating(true)}
+                    />
+                </div>
+                <div className="overlay-ctn-map">
+                    <FontAwesomeIcon
+                        className="overlay-icon"
+                        icon={faMapMarkerAlt}
+                        onClick={() => history.replace("/map")}
+                    />
+                </div>
+            </MenuOverlay>
+        </div>
+    );
 };
 
 
