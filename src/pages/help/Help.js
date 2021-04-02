e79f2b788816be9f275219ac86faaa4acd7f16a9
Chris <clenart4@kent.edu>
2021-04-02 11:26:57 -0400

    Added debug component for virtual testing oncampus

--- src/pages/help/Help.js
+++ src/pages/help/Help.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import {ProcessedSteps} from './../intro/Intro';
 import './help.scss'
+import { Context } from '../../index';
 
 library.add(faTimes);
 
 function Help({ history }) {
+
+    const { onCampus, setOnCampus } = useContext(Context);
 
     useEffect(() => {
         document.body.classList.add("help-body");
@@ -23,6 +26,17 @@ function Help({ history }) {
             <ol className="rules" type="1">
                 <ProcessedSteps />
             </ol>
+            {process.env.NODE_ENV !== 'production' && <>
+                <label className="debug">
+                Emulate On-Campus Experience:
+                <input
+                    name="isOnCampus"
+                    type="checkbox"
+                    checked={onCampus}
+                    onChange={()=> setOnCampus(isOnCampus => !isOnCampus)}
+                />
+                </label>
+            </>}
             <FontAwesomeIcon 
                 className={["overlay-icon", "closeBtn"].join(' ')}
                 icon={faTimes}
